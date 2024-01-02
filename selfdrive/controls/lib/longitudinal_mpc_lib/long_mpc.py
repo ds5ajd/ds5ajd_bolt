40e21a22002ec8e8a6cc2e38b2b1e420f8fc8751
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2024-01-02 09:47:48 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -70,7 +70,7 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
-MIN_ACCEL = -4.0 #-3.5
+MIN_ACCEL = -3.5 #-3.5
 T_FOLLOW = 1.45
 COMFORT_BRAKE = 2.5
 STOP_DISTANCE = 5.6
