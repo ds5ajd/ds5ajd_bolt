24d95259cb645f6a91331dbe814fdf8700eb2e27
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2024-02-17 23:36:15 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -35,7 +35,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
     # return params.ACCEL_MIN, params.ACCEL_MAX
     accel_max_bp = [10., 20., 30., 50., 70., 80.] #=> [10., 20., 50.]
-    accel_max_v =[0.9, 1.0, 0.85, 0.82, 0.9, 0.95] #=> [0.9, 1.0, 0.95]
+    accel_max_v =[0.9, 0.95, 0.85, 0.82, 0.87, 0.9] #=> [0.9, 1.0, 0.85, 0.82, 0.9, 0.95]
 
     return params.ACCEL_MIN, interp(v_current_kph, accel_max_bp, accel_max_v)
 
